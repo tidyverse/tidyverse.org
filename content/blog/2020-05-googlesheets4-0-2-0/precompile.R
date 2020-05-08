3c4fbab170001e973d3afddd1c879acf87d1bf5d
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-08 13:33:19 -0700

    googlesheets4 blog post (the first ever)

---
+++ content/blog/2020-05-googlesheets4-0-2-0/precompile.R
@@ -0,0 +1,9 @@
+# this post can only be compiled by someone with the right token setup
+
+library(knitr)
+
+knit(
+  # the leading `_` keeps blogdown from re-rendering the original
+  "content/blog/2020-05-googlesheets4-0-2-0/_index.Rmarkdown.orig.Rmarkdown",
+  "content/blog/2020-05-googlesheets4-0-2-0/index.Rmarkdown"
+)
