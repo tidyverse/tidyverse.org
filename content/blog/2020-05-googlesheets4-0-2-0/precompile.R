7612182fc95fa6db60af1d1318bc2c61ce0bcba1
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-09 17:34:32 -0700

    Try our usual `.Rmarkdown` suffix

--- content/blog/2020-05-googlesheets4-0-2-0/precompile.R
+++ content/blog/2020-05-googlesheets4-0-2-0/precompile.R
@@ -3,6 +3,6 @@
 library(knitr)
 
 knit(
-  "content/blog/2020-05-googlesheets4-0-2-0/index.Rmarkdown.orig",
-  "content/blog/2020-05-googlesheets4-0-2-0/index.Rmarkdown"
+  "content/blog/2020-05-googlesheets4-0-2-0/index.Rmarkdown",
+  "content/blog/2020-05-googlesheets4-0-2-0/index.markdown"
 )
